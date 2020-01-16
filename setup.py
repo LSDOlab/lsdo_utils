0bb43e6981e03cd9c3d586ba505bceb819d6836e
John T. Hwang <jhwang@eng.ucsd.edu>
2020-01-16 11:24:01 -0800

    first commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+
+setup(
+    name='lsdo_utils',
+    version='1',
+    packages=[
+        'lsdo_utils',
+    ],
+    install_requires=[
+        'dash==1.2.0',
+        'dash-daq==0.1.0',
+    ],
+)
